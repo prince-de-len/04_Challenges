1367da0f17c59389e04b3747c20000fd528fb021
prince-de-len <144515902+prince-de-len@users.noreply.github.com>
2023-11-22 14:30:08 +0700

    Update translating numbers.cpp
    Added reverse translation

--- translating numbers.cpp
+++ translating numbers.cpp
@@ -1,8 +1,8 @@
 /*************************************
 * Автор: Тимофеев Гордей Евгеньевич. ПИ-231
-* Версия 1
+* Версия 1.1
 * Название: Частота повторений
-* Ссылка: https://onlinegdb.com/Y1yA-VsYOW
+* Ссылка: https://onlinegdb.com/PWnGeDAPN
 ************************************/
 
 
@@ -15,9 +15,9 @@ using namespace std;
 string toRoman(int num) {
   const vector<pair<int, string>> roman = {
     {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"},
-    {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"},
+    {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"},
     {1, "I"}};
-  
+
   string result = "";
   for (const auto &r : roman) {
     while (num >= r.first) {
@@ -25,20 +25,57 @@ string toRoman(int num) {
       num -= r.first;
     }
   }
+  
   return result;
+  
+}
+
+int toArabian(string num) {
+  const vector<pair<int, string>> roman = {
+    {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"},
+    {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"},
+    {1, "I"}};
+
+  int result = 0;
+  for (const auto &r : roman) {
+    while (num.substr(0, r.second.length()) == r.second) {
+      result += r.first;
+      num = num.substr(r.second.length());
+      if (num.empty()) {
+        break;
+      }
+    }
+  }
+  
+  return result;
+  
 }
 
 int main() {
   int number;
-  cout << "Введите число (1-3999): ";
-  cin >> number;
   
+  cout << "Введите арабское число (1-3999): ";
+  cin >> number;
+
   if (number < 1 || number > 3999) {
-    cout << "Число вне допустимого диапазона";
+    cout << "Число вне допустимого диапазона" << endl;
     return 1;
   }
-  
+
   cout << "Римское представление: " << toRoman(number) << endl;
   
+  string number_r;
+  
+  cout << "Введите римское число (I-MMMCMXCIX): ";
+  cin >> number_r;
+
+  if (number_r.substr(0, 4) == "MMMM") {
+   cout << "Число вне допустимого диапазона" << endl;
+    return 1;
+  }
+
+  cout << "Арабское представление: " << toArabian(number_r) << endl;
+
   return 0;
+  
 }
